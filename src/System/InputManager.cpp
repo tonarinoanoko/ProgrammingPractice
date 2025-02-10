8682faf3561cd98489ed17bd3ecaf9d06a9a16b5
tonarinoanoko <tonarinn00@gmail.com>
2025-02-10 11:15:11 +0900

    リファクタ

--- src/System/InputManager.cpp
+++ src/System/InputManager.cpp
@@ -5,23 +5,23 @@
 namespace System{
 void InputManager::Update()
 {
-    _previousKeyState = _currentKeyState;  // 直前のキー状態を保存
-    GetHitKeyStateAll(_currentKeyState.data());  // 現在のキー状態を取得
+    _prev_key_state = _current_key_state;  // 直前のキー状態を保存
+    GetHitKeyStateAll(_current_key_state.data());  // 現在のキー状態を取得
 }
 
-bool InputManager::IsKeyDown(int keyCode) const
+bool InputManager::IsKeyDown(int key_code) const
 {
-    return (_currentKeyState[keyCode] && _previousKeyState[keyCode] == false);
+    return (_current_key_state[key_code] && _prev_key_state[key_code] == false);
 }
 
-bool InputManager::IsKeyUp(int keyCode) const
+bool InputManager::IsKeyUp(int key_code) const
 {
-    return (_currentKeyState[keyCode] == false && _previousKeyState[keyCode]);
+    return (_current_key_state[key_code] == false && _prev_key_state[key_code]);
 }
 
-bool InputManager::IsKeyPressed(int keyCode) const
+bool InputManager::IsKeyPressed(int key_code) const
 {
-    return (_currentKeyState[keyCode]);
+    return (_current_key_state[key_code]);
 }
 
 }  // System
