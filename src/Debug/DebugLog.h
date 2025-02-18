88fdd7591d18a01bfb98902ea745b74b9c4ec83c
tonarinoanoko <tonarinn00@gmail.com>
2025-02-18 09:41:16 +0900

    あさーとの追加

--- src/Debug/DebugLog.h
+++ src/Debug/DebugLog.h
@@ -8,4 +8,11 @@ inline void debugLog(std::string message) {
     std::cout << message << std::endl;
 }
 
+inline void assertLog(bool condition, const std::string& message = "") {
+    if (condition == false) {
+        std::cerr << "Assertion failed: " << message << std::endl;
+        throw std::runtime_error(message);
+    }
+}
+
 }  // Debug
