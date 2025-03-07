e5bf6841cd803a887f726915909b8b5185e79b25
tonarinoanoko <tonarinn00@gmail.com>
2025-03-07 17:32:59 +0900

    パラメーター初期化にモンスターの追加

--- src/Parameter/InitParameter.h
+++ src/Parameter/InitParameter.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "Skill.h"
+#include "Monster.h"
 
 namespace Parameter {
 // 各パラメーターのinstanceを呼んでおく
 inline void initParameter() {
     Skill::instance();
+    Monster::instance();
 }
 }  // namespace Parameter
