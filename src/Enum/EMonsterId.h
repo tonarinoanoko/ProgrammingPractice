a39084ec44eacb147ab5f4b0a6e88cc5f44cd9c6
tonarinoanoko <tonarinn00@gmail.com>
2025-03-07 16:50:07 +0900

    自動で作成されたデータをコミット

---
+++ src/Enum/EMonsterId.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "EnumBase.h"
+
+DEFINE_ENUM(EMonsterId, 
+Slime, 
+Gob, 
+Tomato
+)
---
+++ src/Parameter/Monster.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "ParameterBase.h"
+#include "EMonsterId.h"
+
+namespace Parameter {
+namespace Monster {
+class ParameterData : public ParameterBase<EMonsterId::Enum>
+{
+public:
+    std::string _name;
+    void makeParameter(EMonsterId::Enum id) override;
+};
+
+inline auto& instance() {
+    return Parameters<EMonsterId::Enum, Parameter::Monster::ParameterData>::instance();
+}
+}}  // namespace Parameter::Monster
