568f09c6840bfdb97cf3ce89e9e321a95c3e09b6
tonarinoanoko <tonarinn00@gmail.com>
2025-03-13 09:25:02 +0900

    ダメージの最大倍率を3に設定

--- src/Game/Battle/Calc/Damage.h
+++ src/Game/Battle/Calc/Damage.h
@@ -14,6 +14,7 @@ inline int damage(Character::CharacterData const& actor, Character::CharacterDat
     int a_atk = actor.status().statusInt(EStatus::Enum::Atk);
     int t_def = std::max(1, target.status().statusInt(EStatus::Enum::Def));
     float def_par = static_cast<float>(a_atk) / static_cast<float>(t_def);  // 防御力に対する攻撃力の比率を浮動小数点で計算
+    def_par = std::min(3.0f, def_par);
     float rand_factor = 1.0f + (Utility::Math::random(101) / 100.0f);  // ランダム係数(1.0～2.0)
     float damage = a_atk * rand_factor * def_par;  // ダメージ計算
     int final_damage = static_cast<int>(ceil(damage));  // ダメージの切り上げ
