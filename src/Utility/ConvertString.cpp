7267dd2a254f017b0a9027faafb857839822d148
tonarinoanoko <tonarinn00@gmail.com>
2025-03-14 16:33:28 +0900

    不要な処理のカット

--- src/Utility/ConvertString.cpp
+++ src/Utility/ConvertString.cpp
@@ -1,19 +1,9 @@
 #include "ConvertString.h"
 #include <windows.h>
 
-#include "Debug/DebugLog.h"
-
 
 namespace Utility {
 namespace Conv {
-
-std::vector<TCHAR> convertToTCHAR(const std::string& str)
-{
-    int size_needed = MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, NULL, 0);
-    std::vector<TCHAR> tstr(size_needed);  // TCHARはwchar_tとして扱われる
-    MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, reinterpret_cast<wchar_t*>(tstr.data()), size_needed);
-    return tstr;
-}
 
 std::basic_string<TCHAR> convertToStrTCHAR(const std::string& str) {
 #ifdef UNICODE
--- src/Utility/ConvertString.h
+++ src/Utility/ConvertString.h
@@ -6,7 +6,6 @@
 
 namespace Utility {
 namespace Conv {
-std::vector<TCHAR> convertToTCHAR(const std::string& str);
 std::basic_string<TCHAR> convertToStrTCHAR(const std::string& str);
 
 // 渡した数字を空白桁を追加したstringで返す
