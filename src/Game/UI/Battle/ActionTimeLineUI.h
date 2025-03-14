f945cb3f6176f29adc1fcf2f09260140495b9775
tonarinoanoko <tonarinn00@gmail.com>
2025-03-14 16:52:41 +0900

    タイムライン表示のクールタイムを残りターン数表示に

--- src/Game/UI/Battle/ActionTimeLineUI.h
+++ src/Game/UI/Battle/ActionTimeLineUI.h
@@ -27,7 +27,7 @@ public:
         auto const& entrys = _p_battle_time_line->actionEntrys();
         for(int i = 0; i < entrys.size(); ++i) {
             auto const& entry = entrys[i];
-            std::string disp = Utility::Conv::padWithSpaces(entry._action_time, 3) + "("+ Utility::Conv::padWithSpaces(entry._cool_time, 3) + ") : " + entry._name;
+            std::string disp = Utility::Conv::padWithSpaces(entry._action_time, 3) + "("+ Utility::Conv::padWithSpaces(entry._action_time/entry._cool_time, 2) + ") : " + entry._name;
             DrawString(450, 30 + 20 * i, Utility::Conv::convertToStrTCHAR(disp).c_str(), COLOR_WHITE);
         }
     };
